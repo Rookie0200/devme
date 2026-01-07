b67c80c424b09810177303bf5a6cc279316237e5
Rookie0200 <rookietech0200@gmail.com>
2026-01-07 11:42:23 +0530

    Add: added QnA page content and MDEditor init

--- src/app/(protected)/dashboard/qa/page.tsx
+++ src/app/(protected)/dashboard/qa/page.tsx
@@ -1,9 +1,76 @@
-const qaPageComponent = () => {
-  return (
-    <div>
-      qa page
-    </div>
-  )
-}
+"use client";
 
-export default qaPageComponent 
+import useProject from "@/hooks/use-project";
+import { api } from "@/trpc/react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import AskQuestionCard from "../askQuestionCard";
+import { Fragment, useState } from "react";
+import MDEditor from "@uiw/react-md-editor";
+import CodeReferences from "../codeRefrences";
+
+const QnAPage = () => {
+  const { projectId } = useProject();
+
+  const { data: questions } = api.project.getQuestions.useQuery({ projectId });
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const question = questions ? questions[questionIndex] : null;
+  return (
+    <Sheet>
+      <AskQuestionCard />
+      <div className="h-4"></div>
+      <h1 className="text-xl font-semibold"> Saved Questions</h1>
+      <div className="h-2"></div>
+      <div className="flex flex-col gap-2">
+        {questions?.map((question, index) => {
+          return (
+            <Fragment key={question.id}>
+              <SheetTrigger onClick={() => setQuestionIndex(index)}>
+                <div className="flex items-center gap-4 rounded-lg border bg-white p-4 shadow">
+                  <img
+                    className="rounded-full"
+                    height={30}
+                    width={30}
+                    src={question.user.image || "/default-avatar.png"}
+                    alt="User Avatar"
+                  />
+                  <div className="flex flex-col text-left">
+                    <div className="flex items-center gap-2">
+                      <p className="line-clamp-1 text-lg font-medium text-gray-700">
+                        {question.question}
+                      </p>
+                      <span className="text-xs whitespace-nowrap text-gray-400">
+                        {question.createdAt.toLocaleDateString()}
+                      </span>
+                    </div>
+                    <p className="mt-1 line-clamp-2 max-w-md text-sm text-gray-500">
+                      {question.answer}
+                    </p>
+                  </div>
+                </div>
+              </SheetTrigger>
+            </Fragment>
+          );
+        })}
+      </div>
+      {question && (
+        <SheetContent className="sm:mx-w-[80vw]">
+          <SheetHeader>
+            <SheetTitle>{question.question}</SheetTitle>
+            <MDEditor.Markdown source={question.answer} />
+            <CodeReferences
+              fileReferences={(question.fileReferences ?? []) as any}
+            />
+          </SheetHeader>
+        </SheetContent>
+      )}
+    </Sheet>
+  );
+};
+
+export default QnAPage;
