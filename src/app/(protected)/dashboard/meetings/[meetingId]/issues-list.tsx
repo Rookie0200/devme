42654ef0864db75ed164c7b6d1a1b70979ea7063
Rookie0200 <rookietech0200@gmail.com>
2026-02-14 21:31:11 +0530

    feat: add achive, invite, team members UI and routes

--- src/app/(protected)/dashboard/meetings/[meetingId]/issues-list.tsx
+++ src/app/(protected)/dashboard/meetings/[meetingId]/issues-list.tsx
@@ -1,5 +1,20 @@
-import { api } from "@/trpc/react";
-import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { api, type RouterOutputs } from "@/trpc/react";
+import { Dialog, DialogContent } from "@radix-ui/react-dialog";
+import { VideoIcon } from "lucide-react";
+import React, { useState } from "react";
+import { Button } from "react-day-picker";
 
 type Props = {
   meetingId: string;
@@ -14,9 +29,73 @@ const IssuesList = async ({ meetingId }: Props) => {
 
   return (
     <>
-      <div className=""></div>
+      <div className="p-8">
+        <div className="mx-auto flex max-w-2xl items-center justify-between gap-x-8 border-b pb-6 lg:mx-0 lg:max-w-none">
+          <div className="flex items-center gap-x-6">
+            <div className="rounded-full border bg-white p-3">
+              <VideoIcon className="h-6 w-6" />
+            </div>
+
+            <h1>
+              <div className="text-sm leading-6 text-gray-600">
+                Meeting on {""}
+                {meeting.createdAt.toDateString()}
+              </div>
+              <div className="mt-1 text-base leading-6 font-semibold text-gray-900">
+                {meeting.name}
+              </div>
+            </h1>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-2 sm:grid-cols-3">
+          {meeting.issues.map((issue) => (
+            <IssueCard key={issue.id} issue={issue} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
+
+function IssueCard({
+  issue,
+}: {
+  issue: NonNullable<
+    RouterOutputs["project"]["getMeetingById"]
+  >["issues"][number];
+}) {
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{issue.gist}</DialogTitle>
+            <DialogDescription>{issue.headline}</DialogDescription>
+            <p className="text-gray-600">{issue.headline}</p>
+            <blockquote className="mt-4 border-l-4 border-gray-300 bg-gray-50 p-4">
+              <span className="text-sm text-gray-600">
+                {issue.start} - {issue.end}
+              </span>
+              <p className="leading-relaxed font-medium text-gray-900 italic">
+                {issue.summary}
+              </p>
+            </blockquote>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+      <Card className="relative">
+        <CardHeader>
+          <CardTitle className="text-xl">{issue.gist}</CardTitle>
+          <div className="border-b"></div>
+          <CardDescription>{issue.headline}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button onClick={() => setOpen(true)}>View Issue</Button>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
 
 export default IssuesList;
---
+++ src/components/archive-button.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { api } from "@/trpc/react";
+import React from "react";
+import { Button } from "./ui/button";
+import useProject from "@/hooks/use-project";
+import { toast } from "sonner";
+import useRefetch from "@/hooks/use-refetch";
+
+const ArchiveButton = () => {
+  const { projectId } = useProject();
+  const archiveProject = api.project.archiveProject.useMutation();
+  const refetch = useRefetch();
+  return (
+    <Button
+      disabled={archiveProject.isPending}
+      size="sm"
+      variant="destructive"
+      onClick={() => {
+        const confirm = window.confirm(
+          "Are you sure you want to archive this project?",
+        );
+        if (confirm) {
+          archiveProject.mutate(
+            { projectId },
+            {
+              onSuccess: () => {
+                toast.success("Project archived successfully");
+                refetch();
+              },
+              onError: () => {
+                toast.error("Failed to archive project");
+              },
+            },
+          );
+        }
+      }}
+    >
+      Archive
+    </Button>
+  );
+};
+
+export default ArchiveButton;
